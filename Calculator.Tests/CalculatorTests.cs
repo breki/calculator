1c12d15b92217798e9e00d4d4eeb9e132ab74c92
breki <igor.brejc@gmail.com>
2017-11-21 09:17:26 +0100

    First test case - PressingNumberKeyDisplaysThatNumber

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void PressingNumberKeyDisplaysThatNumber()
+        {
+            int number = PressANumberKey();
+            ExpectNumberOnDisplay(number);
+        }
+    }
+}

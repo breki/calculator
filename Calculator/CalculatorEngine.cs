6b05b59b4a170a209840c345d327d5890b7b1900
breki <igor.brejc@gmail.com>
2017-11-21 10:35:12 +0100

    GREEN: implemented changes in CalculatorEngine to allow test to pass.

--- Calculator/CalculatorEngine.cs
+++ Calculator/CalculatorEngine.cs
@@ -4,12 +4,20 @@ namespace Calculator
 {
     public class CalculatorEngine
     {
+        public CalculatorEngine()
+        {
+            keyChars.Add('0');
+        }
+
         public const char KeyDot = '.';
 
         public IReadOnlyList<char> Display => keyChars;
 
         public void PressKey(char keyChar)
         {
+            if (keyChars.Count == 1 && keyChars[0] == '0')
+                keyChars.Clear();
+
             if (keyChar == KeyDot && keyChars.Contains(KeyDot))
                 return;
 

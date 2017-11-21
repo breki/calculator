e29df4b966db983c7f8840f763602428fb657ee2
breki <igor.brejc@gmail.com>
2017-11-21 09:26:09 +0100

    Created the needed methods in CalculatorEngine as stubs

--- Calculator/CalculatorEngine.cs
+++ Calculator/CalculatorEngine.cs
@@ -2,5 +2,17 @@
 {
     public class CalculatorEngine
     {
+        public int Display
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public void PressKey(int number)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
